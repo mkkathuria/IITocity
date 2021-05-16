ed81448da47368440d9aa0b58c91d93023fb778c
Mukul Kathuria <mkkathuria@gmail.com>
2021-05-16 23:01:03 +0530

    product

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // React is the object and 'react' is the module
 import "./index.css";
 import mytemplate from "./mytemplate.js";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -24,38 +25,7 @@ function App() {
         <div>
           <div className="row center">
             {mytemplate.products.map((product) => (
-              <div key="product._id" className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">Rs. {product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product}></Product>
             ))}
           </div>
         </div>
